d99d9a23381af975be64ee7363e259364e9c41a3
Gorka <4787906+gorka@users.noreply.github.com>
2022-08-28 23:15:42 -0500

    Add SVG helper and replace icons (#10)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,19 @@
 module ApplicationHelper
-  def stars(number)
-   '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
-</svg>'.html_safe * number
+  def rating_stars(rating, max = 5)
+    content_tag(:div, class: "flex inline-block") do
+      svg_icon("star", false) * (max - rating) + svg_icon("star") * rating
+    end
+  end
+
+  def svg_icon(file_name, highlighted=true)
+    styles = class_names("h-6", "w-6", { "text-slate-200": !highlighted })
+
+    file = File.read(Rails.root.join("app", "assets", "icons", "#{file_name}.svg"))
+    doc = Nokogiri::HTML::DocumentFragment.parse(file)
+    svg = doc.at_css "svg"
+
+    svg["class"] = styles
+
+    doc.to_html.html_safe
   end
 end
